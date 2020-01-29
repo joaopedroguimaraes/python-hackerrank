9d7222ec3196d1cd081c6518f8a98a8bd78dc30d
João Pedro Guimarães <joaopedroguimaraes96@gmail.com>
2020-01-29 14:21:24 -0300

    Basic Data Types > Nested Lists

--- Basic_Data_Types/find-second-maximum-number-in-a-list.py
+++ Basic_Data_Types/find-second-maximum-number-in-a-list.py
@@ -1,10 +1,14 @@
 if __name__ == '__main__':
-	n = int(input())
-	# sorted in descending order
-	arr = sorted(map(int, input().split()), reverse=True)
-	second = arr[0]
-	for i in range(0, len(arr)):
-		if arr[i] != second:
-			second = arr[i]
-			break
-	print(second)
+	students = []
+	scores = []
+	for _ in range(int(input())):
+		name = input()
+		score = float(input())
+		students.append([name, score])
+		if score not in scores:
+			scores.append(score)
+
+	second_lowest_score = sorted(scores)[1]
+	students_with_second_lowest_score = sorted([student[0] for student in students if student[1] == second_lowest_score])
+	for student_name in students_with_second_lowest_score:
+		print(student_name)
